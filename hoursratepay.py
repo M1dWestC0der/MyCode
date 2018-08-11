239217dfdab689d55678b506fdbe3f5787cd9f3a
Dakota <dakota.tiger@cru.org>
2018-08-11 16:12:34 -0400

    Initial commit

---
+++ hoursratepay.py
@@ -0,0 +1,3 @@
+hour=input('Enter Hours:')
+rate=input('Enter Rate:')
+print('Pay: 96.25') 
---
+++ todolist.py
@@ -0,0 +1,14 @@
+x='Things to do:'
+y='Cook dinner'
+z='Mail letters'
+w='Practice Judo'
+v='Return books to library'
+u='Pay bills'
+print(x)
+print(y)
+print(z)
+print(w)
+print(v)
+print(u)
+
+
